cd3fe726c31b6da7d4beab9a3338f20de8473681
MiguelAFH <miguel.angel.fh@hotmail.com>
2024-08-06 02:40:15 +0000

    Add parallel script to calculate perplexity

--- hf_ehr/eval/val_ppl.py
+++ hf_ehr/eval/val_ppl.py
@@ -26,7 +26,6 @@ def parse_arguments() -> Namespace:
     parser.add_argument('--path_to_ckpt', type=str, required=True, help='Path to model checkpoint')
     parser.add_argument('--device', type=str, default="cuda", help='Device to run validation on')
     parser.add_argument('--split', type=str, default="val", help='Split on which to calculate PPL')
-    parser.add_argument('--output_json', type=str, default="./ppl.json", help='Path to output json to save results')
     return parser.parse_args()
 
 def patch_config(config: DictConfig) -> None:
@@ -123,9 +122,10 @@ def save_results(results: Dict[str, Any],
 
 def main() -> None:
     args: Namespace = parse_arguments()
-    print("Loading model configuration from checkpoint...")
+    print(f"Loading model configuration from checkpoint: {args.path_to_ckpt}")
     device: str = args.device
     ckpt: str = load_ckpt(args.path_to_ckpt)
+    output_json: str = f"{args.path_to_ckpt}.json"
     model: BaseModel = load_model_from_path(args.path_to_ckpt)
     config: Dict[str, Any] = load_config(ckpt)
     tokenizer: CLMBRTokenizer | DescTokenizer | CookbookTokenizer = load_tokenizer_from_config(config)
@@ -145,109 +145,13 @@ def main() -> None:
         "time_taken_seconds": elapsed_time,
         "num_batches": len(dataloader),
     }
+    results["model_ckpt"] = args.path_to_ckpt
     results["model_conifg"] = OmegaConf.to_container(config, resolve=True)
 
     save_results(results, args.output_path)
     print("Average perplexity: ", results["average_perplexity"])
     print("Stats: ", results["stats"])
-    print(f"Results for {args.split} saved under {args.output_path}.")
+    print(f"Results for {args.split} saved under {output_json}.")
 
 if __name__ == "__main__":
     main()
-
-# {
-#    "main":{
-#       "seed":1,
-#       "path_to_output_dir":"/share/pi/nigam/migufuen/hf_ehr/cache/runs/gpt-base-1024--clmbr-10-epochs/",
-#       "is_force_restart":false
-#    },
-#    "callbacks":{
-#       "early_stopping":{
-#          "metric_mode":"min",
-#          "patience":3
-#       },
-#       "model_ checkpointing":{
-#          "save_top_k_val_loss":1,
-#          "save_most_recent_k":1,
-#          "save_most_recent_every_n_train_steps":10000,
-#          "every_n_train_steps":30000,
-#          "every_n_train_nonPAD_tokens":300000000,
-#          "every_n_flops":10000000000000000,
-#          "is_ run_eval_on_checkpoint":true
-#       }
-#    },
-#    "data":{
-#       "dataset":{
-#          "path_to_femr_extract":"/local-scratch/nigam/hf_ehr/som-rit-phi-starr-prod.starr_omop_cdm5_deid_2023_02_08_extract_v8_no_notes",
-#          "is_debug":false,
-#          "name":"FEMRDataset"
-#       },
-#       "dataloader":{
-#          "mode":"approx",
-#          "batch_size":4,
-#          "approx_batch_sampler":{
-#             "max_tokens":16384,
-#             "bucket_size":100,
-#             "is_random_shuffle_across_buckets":true,
-#             "is_random_shuffle_within_buckets":true
-#          },
-#          "n_workers":4,
-#          "max_lengt h":1024,
-#          "is_truncation_random":true
-#       },
-#       "tokenizer":{
-#          "name":"CLMBRTokenizer",
-#          "path_to_config":"/share/pi/nigam/mwornow/hf_ehr/cache/tokenizers/clmbr_v8/tokenizer_config.json"
-#       }
-#    },
-#    "trainer":{
-#       "accumulate_grad_batches":4,
-#       "g radient_clip_value":1.0,
-#       "gradient_clip_algorithm":"norm",
-#       "devices":[
-#          0
-#       ],
-#       "distributed_backend":"ddp",
-#       "min_epochs":1,
-#       "max_epochs":20,
-#       "limit_train_batches":"None",
-#       "limit_val_batches":"None",
-#       "val_check_interval":0.45,
-#       " check_val_every_n_epoch":1,
-#       "optimizer":{
-#          "type":"AdamW",
-#          "betas":"(0.9, 0.95)",
-#          "weight_decay":0.1,
-#          "lr":0.0002
-#       },
-#       "scheduler":{
-#          "num_warmup_steps":40000,
-#          "num_decay_steps":4000000,
-#          "initial_lr":1e-06,
-#          "final_lr":1e-05
-#       }
-#    },
-#    "logging":{
-#       "wandb":{
-#          "is_wandb":true,
-#          "name":"gpt-base-1024--clmbr-10-epochs",
-#          "is_force_create_wandb_run_from_scratch":false
-#       },
-#       "mlflow":{
-#          "is_mlflow":false,
-#          "name":"None"
-#       },
-#       "is_log_grad_norm":false,
-#       "log_every_n_steps ":1
-#    },
-#    "model":{
-#       "name":"gpt2",
-#       "hf_name":"gpt2",
-#       "config_kwargs":{
-#          "n_positions":1024,
-#          "n_layer":12,
-#          "n_head":12,
-#          "n_embd":768
-#       }
-#    }
-# }
