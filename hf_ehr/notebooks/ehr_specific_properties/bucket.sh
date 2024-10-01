b026320b36eb7481a420dbd05cce24f576fe5e4e
Michael <mwornow98@gmail.com>
2024-10-01 10:47:54 +0000

    bucket fix

--- hf_ehr/notebooks/ehr_specific_properties/bucket.sh
+++ hf_ehr/notebooks/ehr_specific_properties/bucket.sh
@@ -22,5 +22,22 @@ tasks=(
 # Loop through each combination of task and strat
 for task in "${tasks[@]}"; do
 	python bucket.py --task "$task" --strat "$strat" --model clmbr
+	python bucket.py --task "$task" --strat "$strat" --model count
+    python bucket.py --task "$task" --strat "$strat" --model gpt2-base-512--clmbr_train-tokens-total_nonPAD-ckpt_val=2000000000-persist_chunk:last_embed:last
+    python bucket.py --task "$task" --strat "$strat" --model gpt2-base-1024--clmbr_train-tokens-total_nonPAD-ckpt_val=2000000000-persist_chunk:last_embed:last
+    python bucket.py --task "$task" --strat "$strat" --model gpt2-base-2048--clmbr_train-tokens-total_nonPAD-ckpt_val=2000000000-persist_chunk:last_embed:last
+    python bucket.py --task "$task" --strat "$strat" --model gpt2-base-4096--clmbr_train-tokens-total_nonPAD-ckpt_val=2000000000-persist_chunk:last_embed:last
+    python bucket.py --task "$task" --strat "$strat" --model hyena-large-1024--clmbr_train-tokens-total_nonPAD-ckpt_val=2000000000-persist_chunk:last_embed:last
+    python bucket.py --task "$task" --strat "$strat" --model hyena-large-4096--clmbr_train-tokens-total_nonPAD-ckpt_val=2000000000-persist_chunk:last_embed:last
+    python bucket.py --task "$task" --strat "$strat" --model hyena-large-8192--clmbr_train-tokens-total_nonPAD-ckpt_val=2000000000-persist_chunk:last_embed:last
+    python bucket.py --task "$task" --strat "$strat" --model hyena-large-16384--clmbr_train-tokens-total_nonPAD-ckpt_val=2000000000-persist_chunk:last_embed:last
+    python bucket.py --task "$task" --strat "$strat" --model llama-base-512--clmbr_train-tokens-total_nonPAD-ckpt_val=1000000000-persist_chunk:last_embed:last
+    python bucket.py --task "$task" --strat "$strat" --model llama-base-1024--clmbr_train-tokens-total_nonPAD-ckpt_val=1500000000-persist_chunk:last_embed:last
+    python bucket.py --task "$task" --strat "$strat" --model llama-base-2048--clmbr_train-tokens-total_nonPAD-ckpt_val=2000000000-persist_chunk:last_embed:last
+    python bucket.py --task "$task" --strat "$strat" --model llama-base-4096--clmbr_train-tokens-total_nonPAD-ckpt_val=2000000000-persist_chunk:last_embed:last
+    python bucket.py --task "$task" --strat "$strat" --model mamba-tiny-1024--clmbr_train-tokens-total_nonPAD-ckpt_val=2000000000-persist_chunk:last_embed:last
+    python bucket.py --task "$task" --strat "$strat" --model mamba-tiny-4096--clmbr_train-tokens-total_nonPAD-ckpt_val=2000000000-persist_chunk:last_embed:last
+    python bucket.py --task "$task" --strat "$strat" --model mamba-tiny-8192--clmbr_train-tokens-total_nonPAD-ckpt_val=2000000000-persist_chunk:last_embed:last
+    python bucket.py --task "$task" --strat "$strat" --model mamba-tiny-16384--clmbr_train-tokens-total_nonPAD-ckpt_val=2000000000-persist_chunk:last_embed:last
 done
 
